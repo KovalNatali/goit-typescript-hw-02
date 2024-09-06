ff8d9254787ed2f2f58e6069318df6264f531712
KovalNatali <koval.natali.777@gmail.com>
2024-09-06 15:54:44 +0300

    02

--- src/components/Api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const API_KEY = '38386717-8fe577dd1ca73c2d4057f199b';
-
-export const fetchImage = async (query, page) => {
-  const response = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  );
-  return response.data;
-};
--- src/components/ButtonLoad/ButtonLoad.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from './ButtonLoad.styled';
-
-export const ButtonLoad = ({ onClick }) => {
-  return (
-    <Button type="submit" onClick={onClick}>
-      Load more
-    </Button>
-  );
-};
